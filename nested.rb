68344d8f26b3bdebfd4ff6bfd9ffaa33e8872a12
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-01-20 18:01:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,8 @@ def adding_matz
           :languages => ["C"]
         }
      } 
-programmer_hash["yukihiro_matsumoto"][:known_for][:languages]
+programmer_hash["yukihiro_matsumoto"][:known_for] = "Ruby"
+programmer_hash["yukihiro_matsumoto"][:languages] = ["LISP", "C"]
 
 puts programmer_hash
     
