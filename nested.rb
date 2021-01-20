2fed3f107dcea73e12d44c2e190b63c7f9d7a183
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-01-20 20:44:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,9 +82,9 @@ def adding_matz
           :languages => ["C"]
         }
      } 
-programmer_hash[:yukihiro_matsumoto] 
+programmer_hash[:yukihiro_matsumoto] = {}
 
-puts programmer_hash
+return programmer_hash
     
 end
 
